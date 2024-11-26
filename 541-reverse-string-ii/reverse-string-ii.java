ac3f4830ac0a6c574011152f80e1b045bf48efed
Muhammad Hassan Raza <147485527+hassandev03@users.noreply.github.com>
2024-11-26 16:16:20 +0500

    Time: 2 ms (20.35%) | Memory: 44.6 MB (8.15%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.java
+++ 541-reverse-string-ii/reverse-string-ii.java
@@ -1,6 +1,6 @@
 class Solution {
     public String reverseStr(String s, int k) {
-        String revString = "";
+        StringBuilder revString = new StringBuilder();
         int strLen = s.length();
 
         for (int index = 0; index < strLen; index += 2 * k) {
@@ -8,23 +8,23 @@ class Solution {
             int remainStrLen = remainString.length();
 
             if (remainStrLen < k) {
-                revString += reverse(remainString);
+                revString.append(reverse(remainString));
             } else {
                 int twoKStrStopIndex = (index + 2 * k) <= strLen ? (index + 2 * k) : index + k;
-                revString += (reverse(s.substring(index, index + k)) + s.substring(index + k, twoKStrStopIndex));
+                revString.append(reverse(s.substring(index, index + k))).append(s.substring(index + k, twoKStrStopIndex));
             }
         }
 
-        return revString + s.substring(revString.length());
+        return revString.append(s.substring(revString.length())).toString();
     }
 
-    private static String reverse(String str) {
+    private static StringBuilder reverse(String str) {
         StringBuilder reversedStr = new StringBuilder();
 
         for (int i = str.length() - 1; i >= 0; i--) {
             reversedStr.append(str.charAt(i));
         }
 
-        return reversedStr.toString();
+        return reversedStr;
     }
 }
