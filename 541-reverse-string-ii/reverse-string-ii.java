9eab7d5e68a538c8a409f5fc1d2487ce0f26cbca
Muhammad Hassan Raza <147485527+hassandev03@users.noreply.github.com>
2024-11-26 16:19:46 +0500

    Time: 2 ms (20.35%) | Memory: 44.7 MB (7.23%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.java
+++ 541-reverse-string-ii/reverse-string-ii.java
@@ -1,21 +1,22 @@
 class Solution {
     public String reverseStr(String s, int k) {
         StringBuilder revString = new StringBuilder();
-        int strLen = s.length();
+        StringBuilder str = new StringBuilder(s);
+        int strLen = str.length();
 
         for (int index = 0; index < strLen; index += 2 * k) {
-            String remainString = s.substring(index);
+            String remainString = str.substring(index);
             int remainStrLen = remainString.length();
 
             if (remainStrLen < k) {
                 revString.append(reverse(remainString));
             } else {
                 int twoKStrStopIndex = (index + 2 * k) <= strLen ? (index + 2 * k) : index + k;
-                revString.append(reverse(s.substring(index, index + k))).append(s.substring(index + k, twoKStrStopIndex));
+                revString.append(reverse(str.substring(index, index + k))).append(str.substring(index + k, twoKStrStopIndex));
             }
         }
 
-        return revString.append(s.substring(revString.length())).toString();
+        return revString.append(str.substring(revString.length())).toString();
     }
 
     private static StringBuilder reverse(String str) {
