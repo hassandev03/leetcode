bcca47611a3c30ca0365d859b25b49eb73216ff3
Muhammad Hassan Raza <147485527+hassandev03@users.noreply.github.com>
2024-12-01 21:24:58 +0500

    Time: 1 ms (94.94%) | Memory: 46.1 MB (5.73%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int totalSum = 0;
+
+        for (final int num: nums) 
+            totalSum += num;
+
+        
+        int leftSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (2 * leftSum + nums[i] == totalSum)
+                return i;
+
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+}
